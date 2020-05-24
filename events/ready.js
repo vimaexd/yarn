1895e8e81a4dfa05a1d75cf65ccfeac3d2f954bc
etstringy <eddyteddyt@gmail.com>
2020-05-24 03:59:03 +0100

    Change strings from "Gradient" to "Nikki" as part of the rename

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const gradient = require('gradient-string');
 
 module.exports = (client) => {
     console.log("=========================")
-    console.log(gradient.fruit('Gradient Bot ready.'))
+    console.log(gradient.fruit('Nikki Bot ready.'))
     console.log(gradient.morning(`v${process.env.VERSION} | Running in ${process.env.NODE_ENV} mode`))
     console.log(gradient.retro(`By etstringy`))
     client.user.setPresence({ game: { name: 'with the hair dye.' }, status: 'streaming' })
--- index.js
+++ index.js
@@ -60,6 +60,6 @@ if(process.env.NODE_ENV === "development"){
 }
 
 app.listen(port, () => {
-    console.log(gradient.fruit('Gradient Site ready.'))
+    console.log(gradient.fruit('Nikki Site ready.'))
     console.log("=========================")
 })
