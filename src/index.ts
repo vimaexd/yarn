1c082120f8fa91b7795710807c5853181f47e017
etstringy <eddyteddyt@gmail.com>
2020-12-24 00:00:45 +0000

    oH

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ function loadCommands(directory: string): void {
 
             let relativePath;
             if(globals.env === "production"){
-                relativePath = directory.split(path.sep).slice(directory.split(path.sep).indexOf("dist") - 1).join("/")
+                relativePath = directory.split(path.sep).slice(directory.split(path.sep).indexOf("dist")).join("/")
             } else {
                 relativePath = directory.split(path.sep).slice(directory.split(path.sep).indexOf("commands")).join("/")
             }
