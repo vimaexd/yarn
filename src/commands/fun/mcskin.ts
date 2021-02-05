72f63311895c63a444d231e34bf0b9990040937a
etstringy <eddyteddyt@gmail.com>
2021-02-05 15:13:44 +0000

    change!

--- src/commands/fun/mcskin.ts
+++ src/commands/fun/mcskin.ts
@@ -2,6 +2,12 @@ import fetch from "node-fetch";
 import Discord, { MessageEmbed } from "discord.js"
 import Command from "../../classes/Command"
 
+interface MojangUser extends Response {
+    id: string;
+    name: string;
+}
+
+
 const Cmd = new Command({
     enabled: true,
     name: "mcskin",
@@ -10,18 +16,32 @@ const Cmd = new Command({
     usage: "mcskin [username]",
     category: "Fun"
 }, async (client, message, args, globals) => {
-    if(!args[0]) return message.channel.send("Please provide a Minecraft Username!");
 
+    let uuid: string | undefined;
     let username = args[0];
     let opts = args.slice(0);
-    let endpoint: string;
+    let overlay: string = "";
 
-    if(opts.includes("noouter")) endpoint = "/armor/body"
-    else endpoint = "/body"
+    if(opts.includes("noouter")) overlay = "&overlay"
 
+    if(!username) return message.channel.send("Please provide a Minecraft Username!");
+
+    fetch(`https://api.mojang.com/profiles/minecraft`, {
+        body: JSON.stringify([username]),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(userData => {
+        if(!userData[0].id) return message.channel.send("User does not exist!");
+        uuid = userData[0].id
+    })
+    
+    
     let SkinEmbed = new Discord.MessageEmbed()
         .setTitle(`Minecraft Skin for ${username}`)
-        .setImage(`https://minotar.net/${endpoint}/${username}/200.png`)
+        .setImage(`https://crafatar.com/renders/body/${uuid}${overlay}`)
         .setColor(globals.config.embedColors.default)
         .setFooter("Yarn", client.user.displayAvatarURL())
         .setURL(`https://namemc.com/profile/${username}`)
