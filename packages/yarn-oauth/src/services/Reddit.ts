1f039fb1677536e4687de7c581ea27f3607223c3
etstringy <eddyteddyt@gmail.com>
2022-01-16 11:47:52 +0000

    😅 I am a verified dumbass

--- packages/yarn-oauth/src/services/Reddit.ts
+++ packages/yarn-oauth/src/services/Reddit.ts
@@ -1,19 +1,22 @@
 import axios from "axios";
 
-export default async () => {
-  try {
-    const reddit_oauth = await axios.post(
-    "https://www.reddit.com/api/v1/access_token", 
-    "grant_type=client_credentials", 
-    {
-      auth: {
-        username: process.env.REDDIT_CLIENTID,
-        password: process.env.REDDIT_CLIENTSECRET
-      }
-    })
-    if(!reddit_oauth.data.access_token) throw "No reddit bearer token"
-    return `Bearer ${reddit_oauth.data.access_token}`;
-  } catch(err) {
-    console.log("Error renewing Reddit token!")
+export default {
+  name: "reddit",
+  renew: async () => {
+    try {
+      const reddit_oauth = await axios.post(
+      "https://www.reddit.com/api/v1/access_token", 
+      "grant_type=client_credentials", 
+      {
+        auth: {
+          username: process.env.REDDIT_CLIENTID,
+          password: process.env.REDDIT_CLIENTSECRET
+        }
+      })
+      if(!reddit_oauth.data.access_token) throw "No reddit bearer token"
+      return `Bearer ${reddit_oauth.data.access_token}`;
+    } catch(err) {
+      console.log("Error renewing Reddit token!")
+    }
   }
 }
